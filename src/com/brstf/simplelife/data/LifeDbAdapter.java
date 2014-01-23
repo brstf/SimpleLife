13d6363af721598e2704b2bdc03a2be62c443f4e
Brian Stauffer <brianrstauffer@gmail.com>
2014-01-23 14:37:03 -0500

    Cleaned up imports.

--- src/com/brstf/simplelife/data/LifeDbAdapter.java
+++ src/com/brstf/simplelife/data/LifeDbAdapter.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 public class LifeDbAdapter {
 	private final static String DATABASE_NAME = "simplelife";
