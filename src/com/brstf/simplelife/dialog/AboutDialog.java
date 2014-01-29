6c9f71ab03ade8cada84acbd5b51754c6cca1801
Brian Stauffer <brianrstauffer@gmail.com>
2014-01-29 18:45:10 -0500

    Updated about page, added donation button

--- src/com/brstf/simplelife/dialog/AboutDialog.java
+++ src/com/brstf/simplelife/dialog/AboutDialog.java
@@ -2,30 +2,46 @@ package com.brstf.simplelife.dialog;
 
 import com.brstf.simplelife.R;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DialogFragment;
-import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.text.Html;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.util.TypedValue;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class AboutDialog extends DialogFragment {
 
-	public static AlertDialog create(Context context) {
-		final TextView tv = new TextView(context);
+	public static AlertDialog create(final Activity activity) {
+		final View view = activity.getLayoutInflater().inflate(
+				R.layout.dialog_about, null);
+		final TextView tv = (TextView) view.findViewById(R.id.about_tv);
 
-		final SpannableString s = new SpannableString(Html.fromHtml(context
-				.getString(R.string.about_message)));
+		final SpannableString s = new SpannableString(Html.fromHtml(activity
+				.getBaseContext().getString(R.string.about_message)));
 		tv.setText(s);
 		tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18.0f);
-		tv.setPadding(30, 30, 30, 30);
 		tv.setMovementMethod(LinkMovementMethod.getInstance());
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		ImageButton paypal = (ImageButton) view.findViewById(R.id.but_donate);
+		paypal.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				Intent myIntent = new Intent(
+						Intent.ACTION_VIEW,
+						Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=FPNN2J9U7HXTJ&lc=US&item_name=Simple%20Life&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted"));
+				activity.startActivity(myIntent);
+			}
+		});
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
 		builder.setTitle(R.string.about_title);
-		builder.setView(tv);
+		builder.setView(view);
 
 		builder.setNegativeButton(R.string.about_button, null);
 
